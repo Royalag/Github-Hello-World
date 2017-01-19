2e49a3c91f4677fc03990cb93cb511054955e43b
Royalag <Aydinking1@gmail.com>
2017-01-19 17:27:11 +0100

    Finished Hello World

---
+++ Github Hello World/MyProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_Hello_World
+{
+    class MyProgram
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
